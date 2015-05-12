e828c83a5cadb72bb5755e8132c3369f5c0cf1de
Mickaël Floc'hlay <dirtyhenry@gmail.com>
2015-05-12 14:58:52 +0200

    Responsive navbar done

--- _coffeescript/bootstragram.js.coffee
+++ _coffeescript/bootstragram.js.coffee
@@ -6,3 +6,10 @@ jQuery ->
     # Cut the jumbomain at the fold and set the background image
     $('#jumbomain').css('height', ($(window).height() - $('#jumbotron').offset().top) + 'px')
     $('#jumbomain').css('background-image', 'url(' + $('#jumbomain').data('background-url') + ')')
+
+    # Listen for menu being showns to change the button icon.
+    $('#bootstragram-menu-collapse').on('show.bs.collapse', () ->
+        $('#nav-button-icon').removeClass('fa-bars').addClass('fa-close')
+    ).on('hide.bs.collapse', () ->
+        $('#nav-button-icon').addClass('fa-bars').removeClass('fa-close')
+    )
--- _javascript/bootstragram.js
+++ _javascript/bootstragram.js
@@ -2,7 +2,12 @@
   jQuery(function() {
     $('#bsg-debug').append("jQuery has loaded. ");
     $('#jumbomain').css('height', ($(window).height() - $('#jumbotron').offset().top) + 'px');
-    return $('#jumbomain').css('background-image', 'url(' + $('#jumbomain').data('background-url') + ')');
+    $('#jumbomain').css('background-image', 'url(' + $('#jumbomain').data('background-url') + ')');
+    return $('#bootstragram-menu-collapse').on('show.bs.collapse', function() {
+      return $('#nav-button-icon').removeClass('fa-bars').addClass('fa-close');
+    }).on('hide.bs.collapse', function() {
+      return $('#nav-button-icon').addClass('fa-bars').removeClass('fa-close');
+    });
   });
 
 }).call(this);
